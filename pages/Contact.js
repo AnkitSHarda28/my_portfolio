6f9f69911ca6c87cbd7e5d454f59d0eb156b086a
AnkitSHarda28 <Ankitsharda68@gmail.com>
2022-05-14 23:27:44 +1000

    contact

--- pages/Contact.js
+++ pages/Contact.js
@@ -4,64 +4,76 @@ import Image from "next/image";
 
 export default function Contact() {
   return (
-    <div>
-      <Navbar />
-      <div className="mx-44 mt-40 h-auto">
-        <h1 className="lg:text-7xl md:text-5xl mb-5">Contact</h1>
-      </div>
-      <div className="text-center mt-20">
-        <h1 className="lg:text-5xl md:text-3xl mb-10">Let Us Get In Touch</h1>
-        <span className="lg:text-2xl md:text-xl bg-gradient-to-r from-left1 to-right1 text-transparent bg-clip-text ">
-          I am looking for a job as a Front-end/
-          <br />
-          Full-stack Developer. You can contact me <br />
-          on my socials. Hit me up if you have any <br />
-          questions, want a collaboration or just
-          <br /> play a game of LUDO. I’ll try to get back
-          <br /> to you as soon as I can.
-        </span>
-        <h1 className=" lg:text-5xl md:text-3xl mb-10 mt-10">
-          Here are my Socials
-        </h1>
-        <div className="flex space-x-10 items-center justify-center ">
-          <a
-            href="https://www.linkedin.com/in/ankit-sharda-7878541a2/"
-            className="hover:animate-pulse"
-          >
-            <Image src={"/Linkedin.svg"} width={70} height={70} alt="linkdin" />
-          </a>
-          <a
-            href="https://github.com/AnkitSHarda28"
-            className="hover:animate-pulse"
-          >
-            <Image src={"/Github.svg"} width={70} height={70} alt="github" />
-          </a>
-          <a
-            href="https://twitter.com/AnkitSharda19"
-            className="hover:animate-pulse"
-          >
-            <Image src={"/Twitter.svg"} width={70} height={70} alt="twitter" />
-          </a>
-          <a
-            href="https://www.instagram.com/ankit_sharda28/"
-            className="hover:animate-pulse"
-          >
-            <Image src={"/Insta.svg"} width={80} height={80} alt="insta" />
-          </a>
-          <a
-            href="mailto:Ankitsharda68@gmail.com"
-            className="hover:animate-pulse"
-          >
-            <Image src={"/Gmail.svg"} width={70} height={70} alt="mail" />
-          </a>
-          <a
-            href="https://www.facebook.com/ankit.sharda.77/"
-            className="hover:animate-pulse"
-          >
-            <Image src={"/facebook.svg"} width={70} height={70} alt="fb" />
-          </a>
+    <div className="flex flex-col min-h-screen justify-between">
+      <div>
+        <Navbar />
+        <div className="mx-44 mt-40 h-auto">
+          <h1 className="lg:text-7xl md:text-5xl mb-5">Contact</h1>
         </div>
-      </div>
+        <div className="text-center mt-20">
+          <h1 className="lg:text-5xl md:text-3xl mb-10">Let Us Get In Touch</h1>
+          <span className="lg:text-2xl md:text-xl bg-gradient-to-r from-left1 to-right1 text-transparent bg-clip-text ">
+            I am looking for a job as a Front-end/
+            <br />
+            Full-stack Developer. You can contact me <br />
+            on my socials. Hit me up if you have any <br />
+            questions, want a collaboration or just
+            <br /> play a game of LUDO. I’ll try to get back
+            <br /> to you as soon as I can.
+          </span>
+          <h1 className=" lg:text-5xl md:text-3xl mb-10 mt-10">
+            Here are my Socials
+          </h1>
+          <div className="flex space-x-10 items-center justify-center p-5 ">
+            <a
+              href="https://www.linkedin.com/in/ankit-sharda-7878541a2/"
+              className="hover:animate-pulse"
+            >
+              <Image
+                src={"/Linkedin.svg"}
+                width={70}
+                height={70}
+                alt="linkdin"
+              />
+            </a>
+            <a
+              href="https://github.com/AnkitSHarda28"
+              className="hover:animate-pulse"
+            >
+              <Image src={"/Github.svg"} width={70} height={70} alt="github" />
+            </a>
+            <a
+              href="https://twitter.com/AnkitSharda19"
+              className="hover:animate-pulse"
+            >
+              <Image
+                src={"/Twitter.svg"}
+                width={70}
+                height={70}
+                alt="twitter"
+              />
+            </a>
+            <a
+              href="https://www.instagram.com/ankit_sharda28/"
+              className="hover:animate-pulse"
+            >
+              <Image src={"/Insta.svg"} width={80} height={80} alt="insta" />
+            </a>
+            <a
+              href="mailto:Ankitsharda68@gmail.com"
+              className="hover:animate-pulse"
+            >
+              <Image src={"/Gmail.svg"} width={70} height={70} alt="mail" />
+            </a>
+            <a
+              href="https://www.facebook.com/ankit.sharda.77/"
+              className="hover:animate-pulse"
+            >
+              <Image src={"/facebook.svg"} width={70} height={70} alt="fb" />
+            </a>
+          </div>
+        </div>
+      </div>{" "}
       <Footer />
     </div>
   );
