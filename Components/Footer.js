305b25d2e7ad2363a93caa960f47a0f22b38df6e
AnkitSHarda28 <Ankitsharda68@gmail.com>
2022-05-15 12:26:12 +1000

    complete

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="mt-2 text-center items-center  w-screen text-white p-3 bg-gradient-to-r from-right to-left ">
+    <div className="mt-2 xxs:text-xs text-center items-center  w-screen text-white p-3 bg-gradient-to-r from-right to-left ">
       <h1>&copy; All Rights Reserved by Ankit Sharda</h1>
     </div>
   );
