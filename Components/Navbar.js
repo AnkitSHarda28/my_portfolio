43949198d275225a3046db1e489a0ae65556683e
AnkitSHarda28 <Ankitsharda68@gmail.com>
2022-05-15 23:05:18 +1000

    completed

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <>
       <div className="flex sm:flex-col xxs:flex-col justify-between items-center text-white p-5 bg-gradient-to-r from-left to-right">
-        <div className="lg:text-4xl sm:text-3xl xxs:text-sm">
+        <div className="lg:text-4xl ">
           <Link href="/">&lt;Ankit Sharda/&gt;</Link>
         </div>
         <div className="lg:space-x-10 sm:space-x-10 xxs:text-xs xxs:space-x-3 lg:text-3xl sm:text-3xl flex ">
